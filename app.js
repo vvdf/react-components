6911af37adc338fda71501c4d676a212998bdccd
7FFF00 <t42please@gmail.com>
2019-12-30 12:02:23 -0800

    Implement GroceryList, GroceryListItems using react

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 // TODO
+var produce = ['Cucumber', 'Kale', 'Soup', 'Dog Food'];
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hover: false
+    };
+  }
+
+  onHover() {
+    this.setState({
+      hover: !this.state.hover
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+    return <li style={style} 
+      onMouseEnter={this.onHover.bind(this)} 
+      onMouseLeave={this.onHover.bind(this)}>
+        {this.props.item}
+      </li>
+  }
+}
+
+var App = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryList items={produce} />
+  </div>
+);
+
+var GroceryList = (props) => (
+  <ul>
+    {props.items.map((item, key) => (
+      <GroceryListItem item={item} key={key} />
+    ))}
+  </ul>
+);
+
+setInterval(() => {
+  ReactDOM.render(<App />, document.getElementById("app"))
+}, 100);
